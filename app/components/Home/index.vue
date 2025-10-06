038cbc0060fab933bd41c8cb615f4d804acbb07e
Faysal Jamil Zihad <19-40115-1@student.aiub.edu>
2025-10-06 17:13:39 +0600

    set mini configuration

---
+++ app/components/Home/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-red-500">
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is error page
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
