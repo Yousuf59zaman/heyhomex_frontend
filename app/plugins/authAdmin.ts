a0f642e8a6430b67aaecb2ae1527b3a26958dded
Faysal Jamil Zihad <108587235+zihaddi@users.noreply.github.com>
2025-11-11 12:11:27 +0600

    fix firebase

--- app/plugins/authAdmin.ts
+++ app/plugins/authAdmin.ts
@@ -3,5 +3,5 @@ import { fetchAdminCurrentUser, adminUser } from "~/composables/adminAuth";
 export default defineNuxtPlugin(async () => {
     const admin_user: any = adminUser();
     if (admin_user.value !== undefined) return;
-    admin_user.value = await fetchAdminCurrentUser();
+     admin_user.value = await fetchAdminCurrentUser();
 });
--- app/plugins/authCitizen.ts
+++ app/plugins/authCitizen.ts
@@ -3,5 +3,5 @@ import { fetchCitizenCurrentUser, citizenUser } from "~/composables/citizenAuth"
 export default defineNuxtPlugin(async () => {
     const citizen_user: any = citizenUser();
     if (citizen_user.value !== undefined) return;
-    // citizen_user.value = await fetchCitizenCurrentUser();
+    citizen_user.value = await fetchCitizenCurrentUser();
 });
