14315b19f0b18bf30b84763ae070c199c45f251d
Md Yousuf Zaman <usufzaman059@gmail.com>
2025-11-18 13:31:42 +0600

    design 2

--- app/pages/kamaina/property/[id].vue
+++ app/pages/kamaina/property/[id].vue
@@ -662,14 +662,14 @@
                             Videos you might like!
                         </h3>
                     </div>
-                    <div class="p-4 space-y-4">
+                    <div class="p-4 space-y-4 md:grid md:grid-cols-2 md:gap-4 md:space-y-0 lg:flex lg:flex-col lg:space-y-4">
                         <div
                             v-for="video in videos"
                             :key="video.id"
                             class="group cursor-pointer"
                             @click="playSidebarVideo(video)">
                             <!-- Video Thumbnail -->
-                            <div class="relative w-full h-32 mb-3">
+                             <div class="relative w-full h-32 md:h-48 lg:h-32 mb-3">
                                 <img
                                     :src="video.thumbnail"
                                     :alt="video.title"
