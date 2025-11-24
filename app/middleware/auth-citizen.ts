4ce8feb1191a89770c33690ebe61287cc2979f0e
Faysal Jamil Zihad <108587235+zihaddi@users.noreply.github.com>
2025-11-24 11:47:50 +0600

    update middleware

--- app/middleware/auth-citizen.ts
+++ app/middleware/auth-citizen.ts
@@ -1,5 +1,41 @@
-export default defineNuxtRouteMiddleware(async () => {
-	const citizen_user = citizenUser();
-	console.log('ciz' , citizen_user);
-	if (!citizen_user.value) return navigateTo('/', { replace: true });
-});
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const citizen_user = citizenUser()
+    console.log("ciz", citizen_user)
+
+    if (!citizen_user.value) return navigateTo("/", {replace: true})
+
+    const userRole = (citizen_user.value as any)?.data.user_role
+    const userTypeSlug = (citizen_user.value as any)?.data.user_type?.[0]?.slug
+
+    const pathSegment = to.path.split("/")[1]
+
+    const protectedSegments = [
+        "kamaina",
+        "military",
+        "investor",
+        "agent",
+        "advertiser",
+    ]
+
+    if (pathSegment && protectedSegments.includes(pathSegment)) {
+        let allowedSegment: string | null = null
+
+        if (userRole === "agent" || userRole === "advertiser") {
+            allowedSegment = userRole
+        } else if (userTypeSlug) {
+            allowedSegment =
+                userTypeSlug === "kamaaina" ? "kamaina" : userTypeSlug
+        }
+
+        if (allowedSegment) {
+            if (
+                pathSegment !== allowedSegment &&
+                protectedSegments.includes(pathSegment)
+            ) {
+                return navigateTo(`/${allowedSegment}/`, {replace: true})
+            }
+        } else {
+            return navigateTo("/", {replace: true})
+        }
+    }
+})
